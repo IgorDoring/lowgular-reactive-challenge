8cca1186f0c11d7676c756ce7ed992cd9aea84ce
IgorDoring <idoring@tutanota.com>
2024-09-19 12:01:18 -0300

    feat: implemeting sorting by field and order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, inject, signal, Signal } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  signal,
+  Signal,
+  WritableSignal,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserService } from './services/user.service';
 import { UserModel } from './model/user';
@@ -13,20 +20,26 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   userService: UserService = inject(UserService);
-  sortOrder: Signal<string> = signal('asc');
-  sortBy: Signal<string> = signal('email');
+  sortOrder: WritableSignal<string> = signal('asc');
+  sortBy: WritableSignal<string> = signal('email');
   users: Signal<UserModel[]> = computed(() => {
-    return this.userService
-      .getAllUsers()
-      .sort((a: UserModel, b: UserModel) => {
-        if(this.sortBy() === 'lastname'){
-          return this.sortOrder() === 'asc'
-          ? a.name.lastname.localeCompare(b.name.lastname)
-          : b.name.lastname.localeCompare(a.name.lastname)
-        }
+    return this.userService.getAllUsers().sort((a: UserModel, b: UserModel) => {
+      if (this.sortBy() === 'lastname') {
         return this.sortOrder() === 'asc'
+          ? a.name.lastname.localeCompare(b.name.lastname)
+          : b.name.lastname.localeCompare(a.name.lastname);
+      }
+      return this.sortOrder() === 'asc'
         ? a.email.localeCompare(b.email)
-        : b.email.localeCompare(a.email)
-      });
+        : b.email.localeCompare(a.email);
+    });
   });
+
+  changeOrder(order: 'asc' | 'desc') {
+    this.sortOrder.set(order);
+  }
+
+  changeField(field: 'email' | 'lastname') {
+    this.sortBy.set(field);
+  }
 }
